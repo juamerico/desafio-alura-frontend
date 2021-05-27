705300127775bb143a9ee46c8016f79c4aa7cfd8
Juliana Américo <juliana.americo>
2021-05-27 12:47:31 -0300

    Hover no social funcionando!

---
+++ js/style.js
@@ -0,0 +1,11 @@
+let autoLayout = document.querySelector(".auto-layout");
+let projeto = document.querySelector(".projeto");
+let container = document.querySelector(".container");
+
+projeto.addEventListener("mouseover", function() {
+    autoLayout.classList.add("exibe");
+});
+
+projeto.addEventListener("mouseout", function() {
+    autoLayout.classList.remove("exibe");
+});
